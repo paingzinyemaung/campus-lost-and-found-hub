c8967519f91977cd38ee6b20fcdba6f70df851ee
PaingZinYeMaung <paingzinyemaung.pgo@gmail.com>
2026-08-01 18:02:21 +0630

    fixed for item route

---
+++ server/src/features/item/item.route.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import {
+  createItem,
+  deleteItem,
+  getItemById,
+  getItems,
+  updateItem,
+} from './item.controller.js';
+import { protect } from '../../middleware/auth.middleware.js';
+import { upload } from '../../middleware/upload.middleware.js';
+
+const noteRoute = Router();
+
+noteRoute.use(protect);
+
+noteRoute.get('/', getItems);
+noteRoute.get('/:id', getItemById);
+
+// upload.single('image') ကို protect နောက်မှ ထည့်ပေးပါ
+noteRoute.post('/', upload.single('image'), createItem);
+
+noteRoute.put('/:id', updateItem);
+noteRoute.delete('/:id', deleteItem);
+
+export default noteRoute;
